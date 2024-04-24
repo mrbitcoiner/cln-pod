565475c5e14b3455f93a10225d9055d74826c149
_ <_>
2024-04-24 09:01:39 +0000

    * Changed stop script: rpc shutdown instead of SIGTERM

--- volume/scripts/stop.sh
+++ volume/scripts/stop.sh
@@ -4,8 +4,8 @@ set -e
 ####################
 ####################
 shutdown() {
+	lightning-cli stop
 	local pid=$(cat /volume/data/lightning.pid)
-	kill -15 ${pid}
 	while kill -0 ${pid} 1>/dev/null 2>&1; do
 		echo 'waiting cln to stop'
 		sleep 1
