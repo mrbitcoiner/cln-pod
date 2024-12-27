5753d0b93ce796a089d0d7c391c41fbfe6c0f8bf
_ <_>
2024-12-27 14:00:39 +0000

    * upgrade core lightning to v24.11.1

--- Containerfile
+++ Containerfile
@@ -8,8 +8,8 @@ RUN \
 	apt install -y --no-install-recommends \
   autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev \
   python3 python3-pip net-tools zlib1g-dev libsodium-dev gettext \
-  libevent-dev wget python3-poetry pkg-config libpq-dev python3-dev \ 
-  libffi-dev
+  libevent-dev wget python3.11-venv pkg-config libpq-dev python3-dev \ 
+  libffi-dev jq
 
 ADD volume/scripts/build.sh /static/scripts/build.sh
 
@@ -29,33 +29,6 @@ RUN \
 
 ENV PATH=/volume/data:/lightning/lightningd:/lightning/cli:${PATH}
 
-COPY --from=0 /lightning/cli/lightning-cli /lightning/cli/lightning-cli
-COPY --from=0 /lightning/lightningd/lightningd /lightning/lightningd/lightningd
-COPY --from=0 /lightning/tools/hsmtool /lightning/tools/hsmtool
-
-COPY --from=0 \
-	/lightning/lightningd/lightning_channeld \
-	/lightning/lightningd/lightning_closingd \
-	/lightning/lightningd/lightning_connectd \
-	/lightning/lightningd/lightning_dualopend \
-	/lightning/lightningd/lightning_gossipd \
-	/lightning/lightningd/lightning_hsmd \
-	/lightning/lightningd/lightning_onchaind \
-	/lightning/lightningd/lightning_openingd \
-	/lightning/lightningd/lightning_websocketd \
-	/lightning/lightningd/
-
-COPY --from=0 \
-	/lightning/plugins/autoclean \
-	/lightning/plugins/bcli \
-	/lightning/plugins/fetchinvoice \
-	/lightning/plugins/funder \
-	/lightning/plugins/topology \
-	/lightning/plugins/keysend \
-	/lightning/plugins/offers \
-	/lightning/plugins/pay \
-	/lightning/plugins/txprepare \
-	/lightning/plugins/spenderp \
-	/lightning/plugins/
+COPY --from=0 /lightning /lightning
 
 ENTRYPOINT ["/volume/scripts/init.sh"]
--- volume/scripts/build.sh
+++ volume/scripts/build.sh
@@ -3,23 +3,57 @@
 set -e
 ####################
 readonly CLN_REPO='https://github.com/elementsproject/lightning'
-readonly COMMIT_VERSION='v0.11.2'
+readonly COMMIT_VERSION='e6398e5b9aa1d54b42fc61db2ac35558f8e4f38a' # v24.11.1
 ####################
 cln_build() {
-	git clone ${CLN_REPO} /lightning
+	[ -e "/lightning" ] || git clone ${CLN_REPO} /lightning
 	cd /lightning
 	git checkout ${COMMIT_VERSION}
-	poetry run pip3 install --upgrade pip
-	poetry run pip3 install mako
-	poetry run pip3 install mrkd
-	# Force older version of mistune because of breaking changes
-	poetry run pip3 install --force-reinstall -v "mistune==0.8.4"
-	poetry install || true
-	poetry run ./configure
-	poetry run make
+
+	export VENV_PATH='tmp_venv'
+	python3 -m venv ${VENV_PATH}
+	${VENV_PATH}/bin/pip install -U pip setuptools
+	${VENV_PATH}/bin/pip install poetry
+	export POETRY=${VENV_PATH}/bin/poetry
+	${POETRY} install || true
+
+	${POETRY} run ./configure
+	${POETRY} run make
+}
+cln_finalize() {
+	cd /lightning
+
+	! [ -e "/lightning_out" ] || rm -r /lightning_out
+	mkdir /lightning_out
+	mkdir -p /lightning_out/cli
+	mkdir -p /lightning_out/lightningd
+	mkdir -p /lightning_out/tools
+	mkdir -p /lightning_out/plugins
+	mkdir -p /lightning_out/plugins/clnrest
+	mkdir -p /lightning_out/plugins/wss-proxy
+	cp -a cli/lightning-cli /lightning_out/cli/lightning-cli
+	cp -a tools/hsmtool /lightning_out/tools/hsmtool
+	cd lightningd
+	cp -a \
+		lightningd lightning_channeld lightning_closingd lightning_connectd \
+		lightning_dualopend lightning_gossipd lightning_hsmd lightning_onchaind \
+		lightning_openingd lightning_websocketd \
+		/lightning_out/lightningd/
+	cd ../plugins
+	cp -a \
+		autoclean bcli funder topology keysend offers pay txprepare spenderp \
+		chanbackup commando exposesecret recklessrpc recover cln-renepay cln-xpay \
+		cln-askrene sql bookkeeper \
+		/lightning_out/plugins/
+	cp -a clnrest/clnrest /lightning_out/plugins/clnrest/clnrest	
+	cp -a wss-proxy/wss-proxy /lightning_out/plugins/wss-proxy/wss-proxy
+
+	rm /lightnind
+	mv /lightning_out /lightning
 }
 build() {
 	cln_build
+	cln_finalize
 }
 ####################
 build
